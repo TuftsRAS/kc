da40d15c0547ee94808ac3618fe844b80566b368
Omar Soto Fortuño <omarpr@gmail.com>
2018-04-20 15:57:04 -0400

    RESKC-2613: Fix Oracle Scripts for Award Sequencing. (#2861)

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1804_005__fix_sequence_numbers_in_award_tables.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1804_005__fix_sequence_numbers_in_award_tables.sql
@@ -8,22 +8,22 @@
 -- Agreement with this file. If not, please write to license@kuali.co.
 --
 
-update award_cgb `ac` set ac.sequence_number = (select sequence_number from award a where a.award_id = ac.award_id);
-update award_persons `ap` set ap.sequence_number = (select sequence_number from award a where a.award_id = ap.award_id);
-update award_approved_equipment `aae` set aae.sequence_number = (select sequence_number from award a where a.award_id = aae.award_id);
-update award_approved_foreign_travel `aaft` set aaft.sequence_number = (select sequence_number from award a where a.award_id = aaft.award_id);
-update award_cost_share `acs` set acs.sequence_number = (select sequence_number from award a where a.award_id = acs.award_id);
-update award_comment `ac` set ac.sequence_number = (select sequence_number from award a where a.award_id = ac.award_id);
-update award_approved_subawards `aas` set aas.sequence_number = (select sequence_number from award a where a.award_id = aas.award_id);
-update award_idc_rate `air` set air.sequence_number = (select sequence_number from award a where a.award_id = air.award_id);
-update award_report_terms `art` set art.sequence_number = (select sequence_number from award a where a.award_id = art.award_id);
-update award_sponsor_contacts `asc` set asc.sequence_number = (select sequence_number from award a where a.award_id = asc.award_id);
-update award_unit_contacts `auc` set auc.sequence_number = (select sequence_number from award a where a.award_id = auc.award_id);
-update award_sponsor_term `ast` set ast.sequence_number = (select sequence_number from award a where a.award_id = ast.award_id);
-update award_payment_schedule `aps` set aps.sequence_number = (select sequence_number from award a where a.award_id = aps.award_id);
-update award_transferring_sponsor `ats` set ats.sequence_number = (select sequence_number from award a where a.award_id = ats.award_id);
-update award_amt_fna_distribution `aafd` set aafd.sequence_number = (select sequence_number from award a where a.award_id = aafd.award_id);
-update award_amount_info `aai` set aai.sequence_number = (select sequence_number from award a where a.award_id = aai.award_id);
-update award_custom_data `acd` set acd.sequence_number = (select sequence_number from award a where a.award_id = acd.award_id);
-update award_closeout `ac` set ac.sequence_number = (select sequence_number from award a where a.award_id = ac.award_id);
-update award_attachment `aa` set aa.sequence_number = (select sequence_number from award a where a.award_id = aa.award_id);
+update award_cgb set award_cgb.sequence_number = (select sequence_number from award a where a.award_id = award_cgb.award_id);
+update award_persons set award_persons.sequence_number = (select sequence_number from award a where a.award_id = award_persons.award_id);
+update award_approved_equipment set award_approved_equipment.sequence_number = (select sequence_number from award a where a.award_id = award_approved_equipment.award_id);
+update award_approved_foreign_travel set award_approved_foreign_travel.sequence_number = (select sequence_number from award a where a.award_id = award_approved_foreign_travel.award_id);
+update award_cost_share set award_cost_share.sequence_number = (select sequence_number from award a where a.award_id = award_cost_share.award_id);
+update award_comment set award_comment.sequence_number = (select sequence_number from award a where a.award_id = award_comment.award_id);
+update award_approved_subawards set award_approved_subawards.sequence_number = (select sequence_number from award a where a.award_id = award_approved_subawards.award_id);
+update award_idc_rate set award_idc_rate.sequence_number = (select sequence_number from award a where a.award_id = award_idc_rate.award_id);
+update award_report_terms set award_report_terms.sequence_number = (select sequence_number from award a where a.award_id = award_report_terms.award_id);
+update award_sponsor_contacts set award_sponsor_contacts.sequence_number = (select sequence_number from award a where a.award_id = award_sponsor_contacts.award_id);
+update award_unit_contacts set award_unit_contacts.sequence_number = (select sequence_number from award a where a.award_id = award_unit_contacts.award_id);
+update award_sponsor_term set award_sponsor_term.sequence_number = (select sequence_number from award a where a.award_id = award_sponsor_term.award_id);
+update award_payment_schedule set award_payment_schedule.sequence_number = (select sequence_number from award a where a.award_id = award_payment_schedule.award_id);
+update award_transferring_sponsor set award_transferring_sponsor.sequence_number = (select sequence_number from award a where a.award_id = award_transferring_sponsor.award_id);
+update award_amt_fna_distribution set award_amt_fna_distribution.sequence_number = (select sequence_number from award a where a.award_id = award_amt_fna_distribution.award_id);
+update award_amount_info set award_amount_info.sequence_number = (select sequence_number from award a where a.award_id = award_amount_info.award_id);
+update award_custom_data set award_custom_data.sequence_number = (select sequence_number from award a where a.award_id = award_custom_data.award_id);
+update award_closeout set award_closeout.sequence_number = (select sequence_number from award a where a.award_id = award_closeout.award_id);
+update award_attachment set award_attachment.sequence_number = (select sequence_number from award a where a.award_id = award_attachment.award_id);
