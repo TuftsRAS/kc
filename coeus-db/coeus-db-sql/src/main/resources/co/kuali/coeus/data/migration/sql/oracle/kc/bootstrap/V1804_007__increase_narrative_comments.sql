50ca9b4ffb2e67288f95fa213e52555e0e596ad1
timbui1 <33756867+timbui1@users.noreply.github.com>
2018-04-24 16:27:09 -0700

    RESKC-2881: Fix Narrative comments not large enough to hold Route Annotation. (#2855)

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1804_007__increase_narrative_comments.sql
@@ -0,0 +1,15 @@
+--
+-- Copyright © 2005-2018 Kuali, Inc.
+-- All Rights Reserved
+-- You may use and modify this code under the terms of the Kuali, Inc.
+-- Pre-Release License Agreement. You may not distribute it.
+--
+-- You should have received a copy of the Kuali, Inc. Pre-Release License
+-- Agreement with this file. If not, please write to license@kuali.co.
+--
+
+ALTER TABLE krew_actn_tkn_t
+  MODIFY annotn varchar2(4000);
+  
+ALTER TABLE narrative
+  MODIFY comments varchar2(4000);
