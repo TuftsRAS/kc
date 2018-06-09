b9d1282fae662f942cdb5f257a946c2645486b19
Gayathri Athreya <gayathri.athreya@kuali.co>
2018-06-08 21:56:30 -0700

    Trying to fix integration tests (#2928)

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1806_007__RESKC-2956.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1806_007__RESKC-2956.sql
@@ -8,10 +8,7 @@
 -- Agreement with this file. If not, please write to license@kuali.co.
 --
 
-update krcr_parm_t set nmspc_cd = 'KC-GEN'where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_EXPRESSION';
-
-update krcr_parm_t set nmspc_cd = 'KC-GEN'where parm_nm = 'WORKFLOW_SIMULATION_JOB_ON';
-
-update krcr_parm_t set nmspc_cd = 'KC-GEN'where parm_nm = 'SAVE_WORKFLOW_SIMULATION_RESULT_ON_WORKFLOW_ACTION';
-
-update krcr_parm_t set nmspc_cd = 'KC-GEN'where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_START_TIME';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_EXPRESSION';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_JOB_ON';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'SAVE_WORKFLOW_SIMULATION_RESULT_ON_WORKFLOW_ACTION';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_START_TIME';
