b0801314c81e034dcff9f584b429dc9d8bf98f1b
Gayathri Athreya <gayathri.athreya@kuali.co>
2018-06-11 17:13:29 -0700

    Lock timeout issue fix (#2931)

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1806_007__RESKC-2956.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1806_007__RESKC-2956.sql
@@ -8,7 +8,7 @@
 -- Agreement with this file. If not, please write to license@kuali.co.
 --
 
-update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_EXPRESSION';
-update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_JOB_ON';
-update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'SAVE_WORKFLOW_SIMULATION_RESULT_ON_WORKFLOW_ACTION';
-update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_START_TIME';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_EXPRESSION' and nmspc_cd = 'KC-COMMON' and cmpnt_cd = 'Document';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_JOB_ON' and nmspc_cd = 'KC-COMMON' and cmpnt_cd = 'Document';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'SAVE_WORKFLOW_SIMULATION_RESULT_ON_WORKFLOW_ACTION' and nmspc_cd = 'KC-COMMON' and cmpnt_cd = 'Document';
+update krcr_parm_t set nmspc_cd = 'KC-GEN' where parm_nm = 'WORKFLOW_SIMULATION_BATCH_JOB_CRON_START_TIME' and nmspc_cd = 'KC-COMMON' and cmpnt_cd = 'Document';
