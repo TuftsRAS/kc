7629afe1f643bb6787f06cadb2a3795b3dfc90e1
Travis Schneeberger <leo.herbie@gmail.com>
2018-07-21 13:37:06 -0400

    RESKC-2339: making cfda number not null in S2S Opportunity

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1807_006__multi_cfda.sql
@@ -0,0 +1,12 @@
+--
+-- Copyright © 2005-2018 Kuali, Inc.
+-- All Rights Reserved
+-- You may use and modify this code under the terms of the Kuali, Inc.
+-- Pre-Release License Agreement. You may not distribute it.
+--
+-- You should have received a copy of the Kuali, Inc. Pre-Release License
+-- Agreement with this file. If not, please write to license@kuali.co.
+--
+
+ALTER TABLE S2S_OPPORTUNITY_CFDA
+  MODIFY CFDA_NUMBER VARCHAR2(7) NOT NULL;
