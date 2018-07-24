94e39347768e4a2d882320952945e7b632cb5038
Omar Soto Fortuño <omarpr@gmail.com>
2018-07-24 10:57:45 -0400

    RESKC-2732: Enable parameter Enable_Lock_On_Document_Version, as preparation for facing it out. (#2986)

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/V1807_007__enable_param_Enable_Lock_On_Document_Version.sql
@@ -0,0 +1,12 @@
+--
+-- Copyright © 2005-2018 Kuali, Inc.
+-- All Rights Reserved
+-- You may use and modify this code under the terms of the Kuali, Inc.
+-- Pre-Release License Agreement. You may not distribute it.
+--
+-- You should have received a copy of the Kuali, Inc. Pre-Release License
+-- Agreement with this file. If not, please write to license@kuali.co.
+--
+
+UPDATE krcr_parm_t SET VAL='Y'
+	WHERE NMSPC_CD='KC-GEN' AND CMPNT_CD='Document' AND PARM_NM='Enable_Lock_On_Document_Version';
